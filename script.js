3db36089a9b5faa8233250ed00f2e9a02bcbee09
AsthaKhurana <astha@wireless-10-104-167-136.umd.edu>
2018-12-09 15:16:52 -0500

    Getting rid of authorization requirement

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 /**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+
+/**
  * Print all the events in the spreadsheet:
  * https://docs.google.com/spreadsheets/d/1jdAx-yiYrLM30_5wKO23fRz7L8s-JdvKDd07PizG8II/edit
  */
